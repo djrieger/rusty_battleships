415f12cff3fc102fe6ffd09cdbf17f973c634e31
David Rieger <djrieger@users.noreply.github.com>
2016-01-23 13:00:41 +0100

    Implemented and tested parsing of all Request messages, works

--- rusty_battleships/src/bin/server.rs
+++ rusty_battleships/src/bin/server.rs
@@ -14,21 +14,31 @@ mod Message;
  * In SERVER mode, the target port for the TCP socket is required.
  */
 
-fn extract_string(reader: &mut BufReader<TcpStream>) -> String {
-    let mut message_buffer:[u8;1] = [0;1];
-    let result = reader.read_exact(&mut message_buffer);
-    let strlen = message_buffer[0];
+fn extract_string(mut reader: &mut BufReader<TcpStream>) -> String {
+    let strlen = extract_number(&mut reader);
 
     let mut string_buffer = vec![];
     let mut chunk = reader.take(strlen as u64);
     let status = chunk.read_to_end(&mut string_buffer);
-    // reader.read_exact(&mut string_buffer);
     return str::from_utf8(& string_buffer).unwrap().to_string();
 }
 
-fn login(mut reader: &mut BufReader<TcpStream>) -> Message::Request {
-    let nickname = extract_string(&mut reader);
-    return Message::Request::Login { username: nickname };
+fn extract_number(reader: &mut BufReader<TcpStream>) -> u8 {
+    let mut message_buffer:[u8;1] = [0;1];
+    let result = reader.read_exact(&mut message_buffer);
+    return message_buffer[0];
+}
+
+fn extract_placement(mut reader: &mut BufReader<TcpStream>) -> [Message::ShipPlacement; 5] {
+    let mut placement:[Message::ShipPlacement; 5] = [Message::ShipPlacement { x: 0, y: 0, direction: Message::Direction::North }; 5];
+    for i in 0..4 {
+        placement[i] = Message::ShipPlacement {
+            x: extract_number(&mut reader),
+            y: extract_number(&mut reader),
+            direction: Message::get_direction(extract_number(&mut reader)),
+        };
+    }
+    return placement;
 }
 
 fn main() {
@@ -55,21 +65,42 @@ fn main() {
         let msg;
         match opcode {
             000 => msg = Some(Message::Request::GetFeatures),
-            001 => msg = Some(login(&mut buff_reader)),
+            001 => msg = Some(Message::Request::Login {
+                username: extract_string(&mut buff_reader)
+            }),
             002 => msg = Some(Message::Request::Ready),
             003 => msg = Some(Message::Request::NotReady),
-            004 => msg = Some(Message::Request::ChallengePlayer { username: extract_string(&mut buff_reader) }),
+            004 => msg = Some(Message::Request::ChallengePlayer {
+                username: extract_string(&mut buff_reader)
+            }),
+            010 => msg = Some(Message::Request::PlaceShips {
+                placement: extract_placement(&mut buff_reader)
+            }),
+            11 => msg = Some(Message::Request::Shoot {
+                x: extract_number(&mut buff_reader),
+                y: extract_number(&mut buff_reader)
+            }),
+            012 => msg = Some(Message::Request::MoveAndShoot {
+                id: extract_number(&mut buff_reader),
+                direction: Message::get_direction(extract_number(&mut buff_reader)),
+                x: extract_number(&mut buff_reader),
+                y: extract_number(&mut buff_reader)
+            }),
+            013 => msg = Some(Message::Request::Surrender),
+            014 => msg = Some(Message::Request::ReportError {
+                errormessage: extract_string(&mut buff_reader) 
+            }), 
             _   => msg = None
         }
-        // println!("{:?}", msg);
-        match msg.unwrap() {
-            Message::Request::GetFeatures => println!("FEATIU"),
-            Message::Request::ChallengePlayer { username: nickname } => println!("{}", nickname),
-            _ => println!("other")
-        }
-        match result {
-            Result::Ok(_) => println!("Received message: {}", str::from_utf8(&	message_buffer).unwrap()),
-            Result::Err(str) => println!("ERROR!")
-        }
+        println!("Received {:?} message", msg);
+        // match msg.unwrap() {
+        //     Message::Request::GetFeatures => println!("FEATIU"),
+        //     Message::Request::ChallengePlayer { username: nickname } => println!("{}", nickname),
+        //     _ => println!("other")
+        // }
+        // match result {
+        //     Result::Ok(_) => println!("Received message: {}", str::from_utf8(&	message_buffer).unwrap()),
+        //     Result::Err(str) => println!("ERROR!")
+        // }
     }
 }
