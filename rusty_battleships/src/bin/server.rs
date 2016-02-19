7e885b4eed95e5b3cd10af22a92a42a2f88a420e
Moritz Baumann <moritz.baumann@student.kit.edu>
2016-02-19 15:20:22 +0100

    Server: Let main thread wait for periodic tick.

--- rusty_battleships/src/bin/server.rs
+++ rusty_battleships/src/bin/server.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::io::{BufReader, BufWriter, Write};
 use std::net::{Ipv4Addr, TcpListener, TcpStream};
 use std::option::Option::None;
 use std::sync::mpsc;
+use std::time::Duration;
 use std::thread;
 
 extern crate time;
@@ -14,7 +15,7 @@ extern crate rusty_battleships;
 use rusty_battleships::message::{serialize_message, deserialize_message, Message};
 use rusty_battleships::board;
 use rusty_battleships::board::{Player, ToMainThreadCommand, ToChildCommand};
-use rusty_battleships::game::{Game, GameState};
+use rusty_battleships::game::Game;
 use rusty_battleships::serverstate;
 
 // http://stackoverflow.com/questions/35157399/how-to-concatenate-static-strings-in-rust/35159310
@@ -26,6 +27,23 @@ macro_rules! version {
 }
 macro_rules! version_string {
     () => ( concat!(description!(), " v", version!()) )
+}
+
+const TICK_DURATION_MS: u64 = 250;
+
+
+fn timer_periodic(ms: u64) -> mpsc::Receiver<()> {
+    let (tx, rx) = mpsc::channel();
+    let duration = Duration::from_millis(ms);
+    std::thread::spawn(move || {
+        loop {
+            std::thread::sleep(duration);
+            if tx.send(()).is_err() {
+                break;
+            }
+        }
+    });
+    rx
 }
 
 // Tell server to perform propert shutdown, like removing player from their game, informing
@@ -41,6 +59,7 @@ fn handle_client(stream: TcpStream, tx: mpsc::SyncSender<ToMainThreadCommand>, r
     let response_stream = stream.try_clone().unwrap();
     let mut buff_reader = BufReader::new(stream);
     let mut buff_writer = BufWriter::new(response_stream);
+    let tick = timer_periodic(TICK_DURATION_MS);
     loop {
         let request = deserialize_message(&mut buff_reader);
         let response_msg;
@@ -57,7 +76,7 @@ fn handle_client(stream: TcpStream, tx: mpsc::SyncSender<ToMainThreadCommand>, r
                             shutdown_player(&tx, &rx);
                         }
                     },
-                    ToChildCommand::TerminateConnection => return, 
+                    ToChildCommand::TerminateConnection => return,
                 }
             },
             // Normal connection termination
@@ -68,7 +87,7 @@ fn handle_client(stream: TcpStream, tx: mpsc::SyncSender<ToMainThreadCommand>, r
                 return;
             },
             // Malformed message received
-            Err(e) => {
+            Err(_) => {
                 shutdown_player(&tx, &rx);
                 println!("Received malformed message, terminating client");
                 response_msg = Message::InvalidRequestResponse;
@@ -82,6 +101,8 @@ fn handle_client(stream: TcpStream, tx: mpsc::SyncSender<ToMainThreadCommand>, r
         if clone_response == Message::InvalidRequestResponse {
             return;
         }
+
+        tick.recv().expect("Timer thread died unexpectedly."); // wait for next tick
     }
 }
 
@@ -97,15 +118,15 @@ fn handle_main(msg: Message, player: &mut board::PlayerHandle, lobby: &mut HashM
     // their nickname must be None
     if player.nickname.is_none() {
         if let Message::LoginRequest { username } = msg {
-            return serverstate::handle_login_request(username, player, lobby); 
+            return serverstate::handle_login_request(username, player, lobby);
         }
     } else {
         // All other requests are only valid after logging in, i.e. with a user name
         match msg {
-            Message::LoginRequest { username } => return serverstate::handle_login_request(username, player, lobby), 
+            Message::LoginRequest { username } => return serverstate::handle_login_request(username, player, lobby),
             Message::ReadyRequest => return serverstate::handle_ready_request(player, lobby),
             Message::NotReadyRequest => return serverstate::handle_not_ready_request(player, lobby),
-            Message::ChallengePlayerRequest { username } => return serverstate::handle_challenge_player_request(username, player, lobby, games),  
+            Message::ChallengePlayerRequest { username } => return serverstate::handle_challenge_player_request(username, player, lobby, games),
             Message::SurrenderRequest => return serverstate::handle_surrender_request(player, lobby),
             Message::PlaceShipsRequest { placement } => return serverstate::handle_place_ships_request(placement, player, lobby),
             Message::ShootRequest { x, y } => return serverstate::handle_move_shoot_request((x, y), None, player, lobby),
@@ -205,7 +226,7 @@ fn main() {
             }
         }
         // Send all messages saved in message_store
-        for (i, player_handle) in player_handles.iter_mut().enumerate() {
+        for player_handle in player_handles.iter_mut() {
             if let Some(ref name) = player_handle.nickname {
                 if message_store.contains_key(name) {
                     for message in message_store.remove(name).unwrap() {
