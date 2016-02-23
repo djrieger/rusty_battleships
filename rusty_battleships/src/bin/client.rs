b923299c4952b2bffadc55a8ff2900ba35530a0e
David Rieger <djrieger@users.noreply.github.com>
2016-02-23 21:33:06 +0100

    Extended old client a bit in order to be able to test server

--- rusty_battleships/src/bin/client.rs
+++ rusty_battleships/src/bin/client.rs
@@ -1,5 +1,6 @@
 use std::net::{Ipv4Addr, TcpStream};
 use std::io::{BufReader, BufWriter, Write};
+use std::io::{self,BufRead};
 use std::option::Option::None;
 
 extern crate argparse;
@@ -22,16 +23,19 @@ macro_rules! version_string {
 fn send_message(msg: Message, stream: &mut BufWriter<TcpStream>) {
     let serialized_msg = serialize_message(msg);
     stream.write(&serialized_msg[..]).unwrap();
+    stream.flush();
 }
 
 fn main() {
     let mut port:u16 = 5000;
     let mut ip = Ipv4Addr::new(127, 0, 0, 1);
+    let mut nickname = String::new();
 
     {  // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
         ap.set_description(description!());
         ap.refer(&mut ip).add_argument("IP", Store, "IPv4 address");
+        // ap.refer(&mut nickname).add_option(&["-n", "--nickname"], Store, "nickname");
         ap.refer(&mut port).add_option(&["-p", "--port"], Store, "port the server listens on");
         ap.add_option(&["-v", "--version"], Print(version_string!().to_owned()),
             "show version number");
@@ -49,11 +53,24 @@ fn main() {
     let mut buff_writer = BufWriter::new(sender);
     let mut buff_reader = BufReader::new(receiver);
 
+    let mut stdin = std::io::stdin();
+
     send_message(Message::GetFeaturesRequest, &mut buff_writer);
-    send_message(Message::LoginRequest { username: String::from("foobar") }, &mut buff_writer);
-    buff_writer.flush();
-    loop {
-        let server_response = deserialize_message(&mut buff_reader).unwrap();
-        println!("{:?}", server_response);
+    let nickname = stdin.lock().lines().next().unwrap().unwrap();
+    send_message(Message::LoginRequest { username: nickname }, &mut buff_writer);
+    send_message(Message::ReadyRequest, &mut buff_writer);
+    stdin = std::io::stdin();
+    let opponent = stdin.lock().lines().next().unwrap().unwrap();
+    if !opponent.is_empty() {
+        send_message(Message::ChallengePlayerRequest { username: opponent }, &mut buff_writer);
     }
+
+    loop {
+        let server_response = deserialize_message(&mut buff_reader);
+        if server_response.is_err() {
+            return;
+        }
+        println!("{:?}", server_response.unwrap());
+        }
+    println!("test");
 }
