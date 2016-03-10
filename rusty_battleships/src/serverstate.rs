b58f4aa470ecfce34f53a59b8b282d708cfa3f5a
David Rieger <djrieger@users.noreply.github.com>
2016-03-10 14:17:42 +0100

    More logging

--- rusty_battleships/src/serverstate.rs
+++ rusty_battleships/src/serverstate.rs
@@ -434,7 +434,7 @@ fn handle_shoot(games: &mut Vec<Rc<RefCell<Game>>>, game: Rc<RefCell<Game>>,
 
         {
             let ref mut opponent_board = if game_ref.player1 != *player_name { &mut game_ref.board2 } else { &mut game_ref.board1 };
-            println!("Shooting on {}'s board:", opponent_name);
+            println!("Shooting on {}'s board at {}:{}:", opponent_name, target_x, target_y);
             opponent_board.compute_state();
             hit_result = opponent_board.hit(target_x as usize, target_y as usize);
             game_over = opponent_board.is_dead();
